f873e37256c58888ccd4c06cf72fd8f9d312f4e6
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2019-09-21 19:35:57 +0530

    Add files via upload

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Vibhor Gupta on 12/20/18.
+//  Copyright © 2018 Vibhor Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
